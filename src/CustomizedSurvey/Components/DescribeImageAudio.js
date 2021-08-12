9983c55f6a18273c7af991f0e7fa070dd677922b
piyush <piyush115dubey@gmail.com>
2021-08-12 16:32:37 +0530

    Added slideshow

--- src/CustomizedSurvey/Components/DescribeImageAudio.js
+++ src/CustomizedSurvey/Components/DescribeImageAudio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as Survey from "survey-react";
 import { ZiggeoRecorder } from "react-ziggeo";
+import SlideShow from './../../common/SlideShow';
 
 const TYPE_NAME = "ziggeodescribeimageonlyaudio";
 
@@ -94,7 +95,11 @@ export class DescribeImageOnlyAudio extends Survey.SurveyElementBase {
                 <div>
                     <b>{text}</b>
                 </div>
-                <img src={url} alt={text} height={height} width={width} />
+                {
+                    Array.isArray(url) ? 
+                    <SlideShow images={url} alt={text} height={height} width={width} /> :
+                    <img src={url} alt={text} height={height} width={width}/> 
+                }
                 <ZiggeoRecorder
                     onlyaudio={true}
                     application={API_KEY}
